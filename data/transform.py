060dbdc6b58a530921a97d902b51ffa9a386cf8a
Rainweic <861514706@qq.com>
2020-02-10 16:46:22 +0800

    Changing the logic of random flipping

--- data/transform.py
+++ data/transform.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 import numpy as np
 import torch
 class Compose(object):
@@ -62,23 +63,14 @@ class ToTensor(object):
                 'label': torch.from_numpy(label)}
 
 class RandomFlip(object):
-    """Randomly flip the input image with a probability of 0.5.
-    
-    Args:
-        axis: 
-            1: Flipped Horizontally
-            0: Flipped Vertically
-            -1: Flipped Horizontally & Vertically 
-
-    """
-    def __init__(self, axis=1):
-        self.axis = axis
+    """Randomly flip the input image with a probability of 0.5."""
+    def __init__(self, flag=0.5):
+        self.flag = flag
 
     def __call__(self, sample):
-        is_flip = np.random.randint(low=0, high=2)
-        if is_flip:
+        if random.uniform(0.0, 1.0) > self.flag:
             image = sample['image']
-            image = cv2.flip(image, self.axis)
+            image = cv2.flip(image, random.randint(0, 1))
             sample['image'] = image
         return sample
 
