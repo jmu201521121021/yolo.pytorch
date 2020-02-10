fe2fdb59f09ea4906e661fb050915176576ac0f2
Rainweic <861514706@qq.com>
2020-02-10 11:10:08 +0800

    Randomly flip the input image with a probability of 0.5.It contains Flipped Horizontally，Flipped Vertically and Flipped Horizontally & Vertically.

--- data/transform.py
+++ data/transform.py
@@ -60,3 +60,25 @@ class ToTensor(object):
         image = image.transpose((2, 0, 1))
         return {'image': torch.from_numpy(image),
                 'label': torch.from_numpy(label)}
+
+class RandomFlip(object):
+    """Randomly flip the input image with a probability of 0.5.
+    
+    Args:
+        axis: 
+            1: Flipped Horizontally
+            0: Flipped Vertically
+            -1: Flipped Horizontally & Vertically 
+
+    """
+    def __init__(self, axis=1):
+        self.axis = axis
+
+    def __call__(self, sample):
+        is_flip = np.random.randint(low=0, high=2)
+        if is_flip:
+            image = sample['image']
+            image = cv2.flip(image, self.axis)
+            sample['image'] = image
+        return sample
+
