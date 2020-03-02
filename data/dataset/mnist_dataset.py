8b24dbf3b88d035042973905730b75105ada9e81
linguanghan <1354009873@qq.com>
2020-03-02 21:38:38 +0800

    add mnist_dataset.py: BuildMnistDataset

--- data/dataset/mnist_dataset.py
+++ data/dataset/mnist_dataset.py
@@ -1,9 +1,5 @@
-import os
-import numpy as np
-import struct
 from data.dataset.base_dataset import BaseDataset
 from data.dataset.imagenet import get_image_list
-from data.dataset.mnist import Mnist
 
 __all__ = ["BuildMnistDataset"]
 
@@ -16,21 +12,5 @@ class BuildMnistDataset(BaseDataset):
         self.setItems(cfg)
 
     def setItems(self, cfg):
-        data_root = cfg.DATASET.DATA_ROOT + os.sep
-
-        # if file train exist, not to produce .txt files and images
-        if (not os.path.exists(data_root + os.sep + 'train')):
-            m = Mnist(data_root)
-            m.convert_to_img(True)
-            m.convert_to_img(False)
-
         self.items = get_image_list(cfg.DATASET.DATA_ROOT, self.training)
 
-
-if __name__  == "__main__":
-    from yolov3.configs.default import get_default_config
-    cfg = get_default_config()
-    cfg.DATASET.DATA_ROOT = "/home/lin/mnist"
-    dataset = BuildMnistDataset(cfg, training=False)
-    for data in enumerate(dataset):
-        print(data)
