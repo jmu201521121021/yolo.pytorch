0f0fa7a60de9a4b7399e1e57875f0c77d0cc5573
lrr <664429371@qq.com>
2020-03-05 13:47:00 +0800

    [update] add init info

--- data/dataset/__init__.py
+++ data/dataset/__init__.py
@@ -3,5 +3,8 @@ from .imagenet import BuildImageNetDataset
 from .build import DATASET_REGISTRY, build_dataset
 from .voc_dataset import BuildVocDataset
 from .coco_dataset import BuildCocoDataset
+from .mnist_dataset import BuildMnistDataset
+from .cifar10_dataset import BuildCifar10Dataset
+
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
