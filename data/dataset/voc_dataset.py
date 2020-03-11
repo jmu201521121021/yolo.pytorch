cd539755ac2430be4020c9403a375d5c395d4e4b
jmu201521121021 <863385256@qq.com>
2020-03-11 23:40:12 +0800

    [update] update voc dataset

--- data/dataset/voc_dataset.py
+++ data/dataset/voc_dataset.py
@@ -51,7 +51,6 @@ class BuildVocDataset(BaseDataset):
     """ Build Coco Dataset"""
     def __init__(self, cfg, traning=True):
         super(BuildVocDataset, self).__init__(cfg, traning)
-        # TODO: read voc2007 and voc2012 dataset
         self.setItems(cfg)
 
     def setItems(self, cfg):
