afa3426518a5099298a500b9dab829d6fc8fee98
linguanghan <1354009873@qq.com>
2020-03-11 12:07:15 +0800

    Delete __init__.py

--- yolov3/modeling/backbone/__init__.py
+++
@@ -1,8 +0,0 @@
-from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
-
-from .backbone import Backbone
-from  .darknet import DarkNetBlockBase, make_stage, DarkNet, build_darknet_backbone
-from  .fpn import build_darknet_fpn_backbone, OutConvLayer, FPN
-from  .ghost_net import build_ghostnet_backbone
-from .mobilenet_v1 import build_mobilenetv1_backbone
-from  .mobilenetV2 import build_mobilenetV2_backbone
